679cc7cbbbf5866fa8a6452dda5da50b31ca246b
rgb <robertobonadeog@gmail.com>
2023-04-10 21:07:15 -0300

    custom hook - useAxios

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { ImSearch } from "react-icons/im";
 import { SearchFormStyled, InputStyled, ButtonStyled } from "./SearchStyles";
 
-const Search = () => {
+const Search = ({ handleSubmit }) => {
+  const [pokemon, setPokemon] = useState()
   return (
     <SearchFormStyled
       onSubmit={(e) => {
         e.preventDefault();
+        handleSubmit(pokemon)
       }}
     >
-      <InputStyled placeholder="Search" />
+      <InputStyled placeholder="Search" value={pokemon} onChange={(e) => setPokemon(e.target.value)} />
 
       <ButtonStyled type="submit">
         <ImSearch size="20" color="#c1c0c9" />
